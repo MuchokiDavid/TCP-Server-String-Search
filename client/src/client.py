b8d4b8577b91a396f7e4f60ab14b357ee97be422
david muchoki <dmmuchoki7@gmail.com>
2025-05-24 23:04:20 +0300

    Refactor client.py to enhance SSL configuration handling and improve code clarity

--- client/src/client.py
+++ client/src/client.py
@@ -1,30 +1,53 @@
+"""
+This script sets up a client socket connection to a server with optional SSL encryption.
+It loads configuration settings, such as host, port, and SSL settings, from a config file.
+The script then creates an SSL context, loads the SSL certificate and private key.
+Establish a socket connection to the server.
+It sends data to the server and receives a response, printing it to the console.
+The script runs in a loop, continuously accepting user input to send to the server.
+
+Parameters:
+    BIND_IP - The IP address to bind the socket to.
+    BIND_PORT - The port to bind the socket to.
+    SSL_ENABLED - A boolean indicating whether SSL encryption is enabled.
+    SSL_CERT - The path to the SSL certificate file.
+    SSL_KEY - The
+"""
+import os
 import socket
 import ssl
 from typing import Optional
+
 from . import config_loader
 
+# Load configuration settings from config file.
 CONFIG: dict = config_loader.load_config()
 BIND_IP: str = CONFIG["host"]
 BIND_PORT: int = CONFIG["port"]
 SSL_ENABLED: bool = CONFIG["ssl_enabled"]
+SSL_CERT: str = CONFIG["ssl_certificate"]
+SSL_KEY: str = CONFIG["ssl_private_key"]
 
+# Get the directory of the current configuration file.
+config_dir: str = os.path.dirname(os.path.abspath(__file__))
+# Determine the project root directory by moving two levels up from the configuration directory.
+project_root: str = os.path.abspath(os.path.join(config_dir, "../.."))
 
-"""
-Create an SSL context with a specified protocol and load the
-certificate chain and key for the server.
-@param ssl.PROTOCOL_TLS_SERVER - The protocol to use for the SSL context.
-@param "../security/server.crt" - The path to the server certificate.
-@param "../security/server.key" - The path to the server key file.
-"""
+# Replace relative paths in the config with absolute paths
+if SSL_CERT.startswith("../"):
+    SSL_CERT = os.path.abspath(os.path.join(project_root, SSL_CERT[3:]))
+
+if SSL_KEY.startswith("../"):
+    SSL_KEY = os.path.abspath(os.path.join(project_root, SSL_KEY[3:]))
+
+# Create an SSL context with a specified protocol and load the certificate.
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 context.check_hostname = False
 context.verify_mode = ssl.CERT_NONE
 context.minimum_version = ssl.TLSVersion.TLSv1_2
 context.maximum_version = ssl.TLSVersion.TLSv1_3
 
-context.load_cert_chain(
-    certfile="../security/server.crt", keyfile="../security/server.key"
-)
+context.load_cert_chain(SSL_CERT, SSL_KEY)
 
 # Create a new socket using the AF_INET address family and SOCK_STREAM socket type.
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -37,11 +60,11 @@ while True:
         try:
             ssock: Optional[ssl.SSLSocket] = None
             if SSL_ENABLED:
+                # Wrap socket to secure the connection when SSL is enabled.
                 ssock = context.wrap_socket(sock, server_hostname=BIND_IP)
                 ssock.sendall(data.encode())
                 response = ssock.recv(1024)
                 print(response.decode())
-
             else:
                 sock.sendall(data.encode())
                 response = sock.recv(1024)
