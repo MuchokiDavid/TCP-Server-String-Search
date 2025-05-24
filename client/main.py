8493b43ad268e17913a894b28d5c8895515f016f
david muchoki <dmmuchoki7@gmail.com>
2025-05-25 01:59:46 +0300

    Enhance docstring in main.py for clarity on client module execution

--- client/main.py
+++ client/main.py
@@ -1,3 +1,7 @@
+"""
+- Import the client module from the src package
+- Run the client's main function if the script is executed as the main program.
+"""
 from src import client
 
 if __name__ == "__main__":
